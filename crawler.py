aeedf78e3fba366d472a2050f394ee263f17764e
Jose Arce <joseangel.epzarce@gmail.com>
2019-01-02 16:16:55 -0600

    standard api search

--- crawler.py
+++ crawler.py
@@ -1,18 +1,23 @@
 from twitter_crawler import get_tweets, get_tweets_ht
+import pandas as pd
 
-
-tweets_gen = get_tweets_ht('banregio', pages=1)
+tweets_gen = get_tweets_ht('banregio', pages=3)
 
 tweets = []
 
 for tweet in tweets_gen:
+    tweet['text'] = tweet['text'].replace("\n", " ")
     tweets.append(tweet)
     for key, value in tweet.items():
         print(key)
         print(value)
         print('----------------------')
-
+tweets_df = pd.DataFrame(tweets)
 print('done!')
+tweets_df_wo_duplicates = tweets_df.drop_duplicates('time')
+tweets_df_wo_duplicates.sort_values(by='time', ascending=False).to_csv('test.csv')
+
+GCP_TWETBR_API_KEY = "AIzaSyA_Bo83CczOiy_jFiknCF99_t3EOcUre7Y"
 
 
 
