cb724c97c052b1143081392c1b6318a667ed5854
Jose Arce <joseangel.epzarce@gmail.com>
2019-01-04 14:19:47 -0600

    historical db

--- tweepy_ext/api.py
+++ tweepy_ext/api.py
@@ -22,3 +22,22 @@ class BRTweepyAPI(tweepy.API):
                            'count', 'include_entities', 'from',
                            'to', 'source', 'fromDate', 'toDate']
         )
+
+    @property
+    def search_fullarchive(self):
+        """ :reference: https://dev.twitter.com/rest/reference/get/search/tweets
+                    :allowed_param:'q', 'lang', 'locale', 'since_id', 'geocode',
+                     'max_id', 'since', 'until', 'result_type', 'count',
+                      'include_entities', 'from', 'to', 'source'
+                """
+        return bind_api(
+            api=self,
+            # path='/tweets/search/30day/brsentimentdev.json',
+            path='/tweets/search/fullarchive/brsentimentfulldev.json',
+            method='POST',
+            payload_type='search_results',
+            allowed_param=['q', 'lang', 'locale', 'since_id', 'geocode',
+                           'max_id', 'since', 'until', 'result_type',
+                           'count', 'include_entities', 'from',
+                           'to', 'source', 'fromDate', 'toDate']
+        )
--- twitter_client.py
+++ twitter_client.py
@@ -53,30 +53,23 @@ def tweet_histories(tweets_id_list, tweets_root_id_list, min_date, max_id=None):
         print(exc)
 
 
-def tweet_histories_premiere(query, tweets_id_list, tweets_root_id_list, max_results, from_date, to_date, next=None):
+def tweet_histories_premiere(query, from_date, to_date, max_results=100,
+                             max_num_req=10, next=False,):
+    with orm.db_session:
+        tweets_id_list = list(orm.select(int(tweet.id) for tweet in Tweet)[:])
     try:
+        post_data = {
+            'query': query,
+            'fromDate': from_date,
+            'toDate': to_date,
+            'maxResults': max_results
+        }
         if next:
-            tweets = api.search_30day(
-                post_data={
-                    "query": query,
-                    'fromDate': from_date,
-                    'toDate': to_date,
-                    'maxResults': max_results,
-                    'next': next
-                },
-                parser=BRModelParser()
-            )
-        else:
-            tweets = api.search_30day(
-                post_data={
-                    "query": query,
-                    'fromDate': from_date,
-                    'toDate': to_date,
-                    'maxResults': max_results
-                },
-                parser=BRModelParser()
-            )
+            post_data['next'] = next
+        # tweets = api.search_30day(post_data=post_data, parser=BRModelParser())
+        tweets = api.search_fullarchive(post_data=post_data, parser=BRModelParser())
 
+        max_num_req -= 1
         for tweet in tweets:
             if hasattr(tweet, 'retweeted_status'):
                 tweet = tweet.retweeted_status
@@ -84,36 +77,43 @@ def tweet_histories_premiere(query, tweets_id_list, tweets_root_id_list, max_res
                 continue
             new_history = [twt for twt in tweet_history(tweet)]
             tweets_id_list.extend([twt.id for twt in new_history])
-            tweets_root_id_list.append({'id': tweet.id, 'created_at': tweet.created_at})
             yield new_history
-        if tweets and tweets.next:
-            yield from tweet_histories_premiere(query, tweets_id_list, tweets_root_id_list,
-                                                max_results, from_date, to_date, tweets.next)
+        if tweets and tweets.next and max_num_req > 0:
+            yield from tweet_histories_premiere(query, from_date, to_date, max_results,
+                                                max_num_req, tweets.next)
     except Exception as exc:
         print(exc)
 
 
 tweets_list = []
-with orm.db_session:
-    tweets_id_list = list(orm.select(int(tweet.id) for tweet in Tweet)[:])  # []
 tweets_root_id_list = []
 histories = []
 min_date = datetime.utcnow() - timedelta(days=14)
 
+
+# max_results = 100
+
 query = "banregio OR #banregio"
-max_results = 100
-from_date = (datetime.utcnow() - timedelta(days=22)).strftime("%Y%m%d%H%M")  # 60 - (60 * 24) * 7
-to_date = (datetime.utcnow() - timedelta(days=18)).strftime("%Y%m%d%H%M")    # / 60 - (60 * 24) * 3
+
+# 30day endpoint
+# from_date = (datetime.utcnow() - timedelta(days=31)).strftime("%Y%m%d%H%M")
+# to_date = (datetime.utcnow() - timedelta(minutes=3)).strftime("%Y%m%d%H%M")
+
+# fullarchive endpont
+orm.set_sql_debug(True)
+with orm.db_session:
+    # query examples
+    # sel = orm.select(twt for twt in Tweet if twt.id not in
+    #                  orm.select(t.in_reply_to_status_id for t in Tweet))
+    #
+    # sel2 = orm.select(twt for twt in Tweet if twt.in_reply_to_status_id == '978778172492828672')[:]
+    to_date_raw = orm.select(twt.created_at for twt in Tweet)[:][-1]
+
+to_date = to_date_raw.strftime("%Y%m%d%H%M")
+from_date = (to_date_raw - timedelta(days=31)).strftime("%Y%m%d%H%M")
 
 
-
-# {'id': 1077932883959009283, 'created_at': datetime.datetime(2018, 12, 26, 14, 23, 21)}
-# {'id': 1076897728339136512, 'created_at': datetime.datetime(2018, 12, 23, 17, 50)}
-
-max_id = None
-# max_id = 1076897728339136512 - 1
-
-for history in tweet_histories_premiere(query, tweets_id_list, tweets_root_id_list, max_results, from_date, to_date):
+for history in tweet_histories_premiere(query, from_date, to_date):
     print(history)
     histories.append(history)
 
